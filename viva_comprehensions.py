4e434e7c4697e5a22dac687fbdbf1bc1f90f514f
leeg <leeg@new-host-6.home>
2020-11-28 20:07:58 -0500

    test cases passing

--- viva_comprehensions.py
+++ viva_comprehensions.py
@@ -9,40 +9,59 @@ class Parity(enum.Enum):
 
 def gen_list(start: int, stop: int, parity: Parity) -> List[int]:
     """
-    Oh no some evil developer decided not to write docstrings. Maybe you can use the test cases to decipher
-    what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
-    updating this here docstring to something useful.
+    Creates a list that starts at the variable start (inclusive) and ends at the
+    variable stop (exclusive). It will give you even or odd numbers, depending on
+    the variable Parity.
 
-    :param start:
-    :param stop:
-    :param parity:
-    :return:
+    :param start: an int representing the start of the range
+    :param stop: an int representing the end of the range
+    :param parity: determines if the number is odd or even
+    :return: list of integers
     """
-    pass
+    if parity == Parity.ODD:
+        x = [value for value in range(start,stop) if value % 2 != 0]
+        return x
+    if parity == Parity.EVEN:
+        x = [value for value in range(start,stop) if value % 2 == 0]
+        return x
+    # pass
 
 
 def gen_dict(start: int, stop: int, strategy: Callable) -> Dict:
     """
-    Oh no some evil developer decided not to write docstrings. Maybe you can use the test cases to decipher
-    what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
-    updating this here docstring to something useful.
+    Prints key value pairs where the value is each key squared.
+    Start is the beginning value, stop is the ending value and
+    factorial signifies how the key will be manipulated to produce the value.
 
 
-    :param start:
-    :param stop:
-    :param strategy:
-    :return:
+    :param start: an int representing the start of the range
+    :param stop: an integer representing the end of the range
+    :param strategy: determines how to manipulate the range
+    :return: Dict
     """
-    pass
+    x = {value : strategy(value) for value in range(start, stop)}
+    return x
+    # pass
 
 
 def gen_set(val_in: str) -> Set:
     """
-    Oh no some evil developer decided not to write docstrings. Maybe you can use the test cases to decipher
-    what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
-    updating this here docstring to something useful.
+    Takes a string and prints one of each lower case letter in uppercase, inside of a dictionary.
 
-    :param val_in:
-    :return:
+    :param val_in: Str
+    :return: Set
     """
-    pass
+    if val_in.islower():
+        a = []
+        for i in val_in:
+            a.append(i.upper())
+        return set(a)
+    elif not val_in.isupper():
+        a = []
+        for i in val_in:
+            if i.islower():
+                a.append(i.upper())
+        return set(a)
+    elif val_in.isupper():
+        return set()
+    # pass
